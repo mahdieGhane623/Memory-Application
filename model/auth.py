1060754439ab3fcb130697e94ce95500f3be035f
mahdieGhane623 <Mahdie@agentiiv.com>
2025-12-08 11:26:17 -0800

    Delete model directory

--- model/auth.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
